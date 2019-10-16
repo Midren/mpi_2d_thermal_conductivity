8d535cfdcfde38707a0b96bb0a7911a0c47d5a4f
Roman Milishchuk <milromchuk@gmail.com>
2019-10-16 16:14:01 +0300

    Refactored

--- main.cpp
+++ main.cpp
@@ -20,24 +20,7 @@ bool check_neumann_criteria(ConductivityAttributes &args) {
     return args.delta_t <= (std::pow(std::max(args.delta_x, args.delta_y), 2) / (4 * alpha));
 }
 
-void update_conductivity(const ConductivityAttributes &args, multiArray &arr, const size_t &from, const size_t &to,
-                         boost::mpi::communicator &world) {
-    //send recv updt
-    double *from_row;
-    double *to_row;
-    size_t w_rank = world.rank();
-    size_t w_size = world.size();
-    if(w_rank != 1) {
-        world.send(w_rank - 1, 0, arr.get_row(from + 1), arr.width());
-        world.recv(w_rank - 1, 0, from_row, arr.width());
-        arr.set_row(from, from_row);
-    }
-    if(w_rank != (w_size - 1)) {
-        world.send(w_rank + 1, 0, arr.get_row(to - 1), arr.width());
-        world.recv(w_rank + 1, 0, to_row, arr.width());
-        arr.set_row(to, to_row);
-    }
-
+void update_conductivity(const ConductivityAttributes &args, multiArray &arr, const size_t &from, const size_t &to) {
     auto alpha = args.conductivity / (args.density * args.heat_capacity);
     static multiArray old_arr = arr;
     arr.swap(old_arr);
@@ -52,7 +35,7 @@ void update_conductivity(const ConductivityAttributes &args, multiArray &arr, co
     }
 }
 
-std::pair<size_t, size_t> range_calc(const size_t &world_size, const size_t &current_proc, const size_t &height ){
+std::pair<size_t, size_t> range_calc(const size_t &world_size, const size_t &current_proc, const size_t &height) {
     size_t start_calc = std::ceil((height / (world_size - 1.0)) * (current_proc - 1));
     size_t end_calc = std::ceil((height / (world_size - 1.0)) * (current_proc));
     start_calc = start_calc == 0 ? 0 : start_calc - 1;
@@ -63,29 +46,48 @@ std::pair<size_t, size_t> range_calc(const size_t &world_size, const size_t &cur
 int main(int argc, char *argv[]) {
     boost::mpi::environment env{argc, argv};
     boost::mpi::communicator world;
+    int w_rank = world.rank();
+    int w_size = world.size();
+
     auto args = getArgs("../conf.txt");
     if (!check_neumann_criteria(args)) {
         throw std::invalid_argument("Arguments doesn't fulfill Neumann criteria");
     }
     auto T = multiArray(args.height, args.width);
     read_initial_data(T, args.input_file);
-//    for (size_t i = 0; i < args.iteration_max; i++) {
-//        update_conductivity(args, T);
+
     if (world.rank() == 0) {
-//        std::cout << "Master" << std::endl;
+
     } else {
-//        std::cout << "Slave" << std::endl;
-        auto test_pair = range_calc(world.size(), world.rank(), args.height);
-        std::cout << "-----------\n" << std::endl
-                  << args.height << "\n" << std::endl
-                  << test_pair.first << " | " << test_pair.second << std::endl;
+        auto[from, to] = range_calc(world.size(), world.rank(), args.height);
+        auto *from_row = new double[T.width()];
+        auto *to_row = new double[T.width()];
+
+        for (auto i = 0; i < args.iteration_max; i++) {
+            update_conductivity(args, T, from, to);
+            if (w_rank != 1) {
+                world.send(w_rank - 1, 0, T.get_row(from + 1), T.width());
+                world.recv(w_rank - 1, 0, from_row, T.width());
+                T.set_row(from, from_row);
+            }
+            if (w_rank != (w_size - 1)) {
+                world.send(w_rank + 1, 0, T.get_row(to - 1), T.width());
+                world.recv(w_rank + 1, 0, to_row, T.width());
+                T.set_row(to, to_row);
+            }
+        }
+        delete[] from_row;
+        delete[] to_row;
+
+        if (world.rank() == 2) {
+            for (int i = 0; i < T.height(); i++) {
+                for (int j = 0; j < T.width(); j++) {
+                    std::cout << T(i, j) << "\t";
+                }
+                std::cout << std::endl;
+            }
+        }
     }
-//    }
-//    for (int i = 0; i < T.height(); i++) {
-//        for (int j = 0; j < T.width(); j++) {
-//            std::cout << T(i, j) << "\t";
-//        }
-//        std::cout << std::endl;
 //    }
     return 0;
 }
